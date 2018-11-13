2542626e04baf02d650a21984c37424d3a3a083c
xNorbertx <norbert.g.e.bakker@gmail.com>
2018-11-13 22:36:00 +0100

    get that kromo gang shit

--- imagecarousel/src/main/resources/static/js/requestHandler.js
+++ imagecarousel/src/main/resources/static/js/requestHandler.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
+    var image = new Image;
     $.ajax({
         url: '/images/test.jpg',
         method: 'GET',
         contentType: "application/octet-stream",
-        success: function(response) {
-            console.log(response);
+        xhr: function() {
+            var xhr = new XMLHttpRequest();
+            xhr.responseType= 'blob'
+            return xhr;
+        },
+        success: function(data) {
+            var img = document.getElementById('img');
+            var url = window.URL || window.webkitURL;
+            img.src = url.createObjectURL(data);
         },
         error: function(error) {
             console.log(error);
